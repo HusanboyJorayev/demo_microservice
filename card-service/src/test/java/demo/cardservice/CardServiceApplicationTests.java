90e713a87764ce856508951eb623fd53e204f40b
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-03-19 20:16:01 +0500

    Initial commit

---
+++ card-service/src/test/java/demo/cardservice/CardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.cardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-registry/src/main/java/demo/userregistry/UserRegistryApplication.java
@@ -0,0 +1,13 @@
+package demo.userregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistryApplication.class, args);
+	}
+
+}
